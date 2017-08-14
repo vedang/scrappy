a143e8223ae56e7fd6a5a79c70cd613f1eae7f7d
Vedang Manerikar <ved.manerikar@gmail.com>
2017-08-14 23:55:28 +0530

    Move logs to a rolling file.
    This lets me inspect threadpools on the repl without getting constantly
    distracted by logging output.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/tools.logging "0.4.0"]
-                 [org.apache.logging.log4j/log4j-api "2.8"]
                  [org.apache.logging.log4j/log4j-core "2.8"]
                  [org.apache.logging.log4j/log4j-slf4j-impl "2.8"]
                  [com.fasterxml.jackson.core/jackson-core "2.8.5"]
