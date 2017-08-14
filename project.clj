adc3ba0c9a038a08104556d3c24d5c360176e31b
Vedang Manerikar <ved.manerikar@gmail.com>
2017-08-14 08:45:20 +0530

    Add log4j2 logging to the project.
    This is the first project I'm trying log4j2 in, so I've taken the
    default config, commented out things I don't need and left the rest the
    way it is. Please ignore.

--- project.clj
+++ project.clj
@@ -3,6 +3,13 @@
   :url "http://vedang.me/techlog/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.apache.logging.log4j/log4j-api "2.8"]
+                 [org.apache.logging.log4j/log4j-core "2.8"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.8"]
+                 [com.fasterxml.jackson.core/jackson-core "2.8.5"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.8.5"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.8.5"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
